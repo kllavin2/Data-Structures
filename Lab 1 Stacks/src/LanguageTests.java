a74efd19fc173fe5002dc8822c982daf0494f993
Kristi <Kristi@JohnBeers-LT>
2017-06-27 22:53:37 -0400

    L4 set up

--- Lab 1 Stacks/src/LanguageTests.java
+++ Lab 1 Stacks/src/LanguageTests.java
@@ -159,9 +159,7 @@ public class LanguageTests {
 		StrStacks L3Stack = new StrStacks(DEFAULT_STACK_SIZE);
 		StrStacks aStack = new StrStacks(DEFAULT_STACK_SIZE);
 		StrStacks bStack = new StrStacks(DEFAULT_STACK_SIZE);
-		StrStacks a2Stack = new StrStacks(DEFAULT_STACK_SIZE);
-				
-				
+							
 		//Populate Stacks
 		for (int index = 0; index < s.length(); index++)
 		{
@@ -208,38 +206,102 @@ public class LanguageTests {
 				bStack.push(c);
 			}
 		}	
-		/*Pop 2 'B' for every 'A' 
-		 * Pop both A and B until empty keep A in another temp stack then pop the temp stack with the remaining bstack if they are null at the same time AnB2n
-		 */
-		while (!aStack.isEmpty())
+	
+		//Disqualify if 'A' does not equal '2B'
+		while (!aStack.isEmpty() || !bStack.isEmpty())
 		{
 			Character a = aStack.pop();
 			Character b = bStack.pop();
-			a2Stack.push(a);
+			Character b2= bStack.pop();
+			if (a == null || b == null || b2== null)
+			{
+				result = false;
+			}
 					
 		}
-		
-		while (!a2Stack.isEmpty() && !bStack.isEmpty())
-		{
-			Character a2 = aStack.pop();
-			Character b = bStack.pop();
-			//if(a2==null || b==null)
-			//{
-				//result=false;
-			//}
 			
-		}
-				
 		return result;
 		
 	}
 	
+	/*
+	 * This function tests whether or not the given string is in L4.
+	 * L4 = { w: w is of the form (AnBm)p, for some m,n,p > 0 } 
+	 * @param s String to test against L1 criteria
+	 * @return result Boolean result of test.
+	 */
+	
 	public static boolean testL4(String s)
 	{
-		boolean result = true;
-		//Do code here
-		System.out.println("TODO: WRITE THIS FUNCTION!");		
+		boolean result = true; //Assume string passes test
+		boolean newStack=true;//Check first item on top of stack
+		boolean poppedLetterA=false;//Check if character is A
+		
+		//Declare stacks
+		StrStacks L3Stack = new StrStacks(DEFAULT_STACK_SIZE);
+		StrStacks aStack = new StrStacks(DEFAULT_STACK_SIZE);
+		StrStacks bStack = new StrStacks(DEFAULT_STACK_SIZE);
+		StrStacks rptStack= new StrStacks(DEFAULT_STACK_SIZE);
+							
+		//Populate Stacks
+		for (int index = 0; index < s.length(); index++)
+		{
+			Character c = s.charAt(index);
+			L3Stack.push(c);
+		}
+		
+		//Run language test		
+		if (L3Stack.isEmpty())
+		{
+			result=false; 
+		}
+		
+		while(!L3Stack.isEmpty())
+		{
+			Character c = L3Stack.pop();
+			
+			//Disqualify if top of fresh stack is not 'B'
+			if(c!='B' && newStack)
+			{
+				result = false;
+			}
+			
+			newStack = false;
+			//Disqualify if top of stack is not 'A' or 'B'
+			if(c!='A'&& c!='B')
+			{
+				result = false; 
+			}
+			//Pushes 'A' and 'B' onto rptStack
+			if (c == 'A' || c == 'B')
+			{
+				rptStack.push(c);
+			}
+		}	
+		
+		while(!rptStack.isEmpty())
+		{
+			Character c = rptStack.pop();
+			if(c == 'A'){
+				
+			}
+		}
+	
+		//Disqualify if 'A' does not equal '2B'
+		while (!aStack.isEmpty() || !bStack.isEmpty())
+		{
+			Character a = aStack.pop();
+			Character b = bStack.pop();
+			Character b2= bStack.pop();
+			if (a == null || b == null || b2== null)
+			{
+				result = false;
+			}
+					
+		}
+			
 		return result;
+		
 	}
 	
 	public static boolean testL5(String s)
