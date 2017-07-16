5677112622cea0cda350ffbaf5891a25262188b8
Kristi <Kristi@JohnBeers-LT>
2017-07-16 12:02:17 -0400

    Retooled Determinant calculation

--- KLavin_Lab2/src/Determinant.java
+++ KLavin_Lab2/src/Determinant.java
@@ -1,62 +1,58 @@
-
-public class Determinant{
-		public void getMinor(int A[][], int temp[][], int n, int p, int q){
-			int i = 0; //row
-			int j = 0; // column
-			int value = 0; //value
-			//Loop to get each element in the matrix
-			for (int i1 = 0; i1 < n; i1++)
-				for (int j1 = 0; j1 < n; j1++){
-					//copy into temporary matrix which are give row or a column
-					if (i1 != p && j1 !=q)
-					temp[i][j++] = A[i1][j1];
-					//Row is filled, increase the row reset the column
-					if (j1 == n-1)
-						j1 = 0;
-						i ++;
-					
-				}
-		}
-		//Recursive function for finding determinant matrix
-		public int determinant(int A[][], int n )
+/*
+ * Calculates the determine of a matrix 6x6 or smaller.
+ */
+public class Determinant
+{
+	int A [][];
+	int M[][];
+	int N;
+		int start;
+		int last;
+		public Determinant(int A[][], int N, int start, int last)
 		{
-			int det = 0;//Initialize the result
-			//Store minors
-			int temp[][] = null;
-			int sign = 1;
-			//Stop case: if the matrix only contains one item
-			if (n == 1)
+			this.A = A;
+			this.N = N;
+			this.start = start;
+			this.last = last;
+		}
+		public int[][] SubArray (int[][] A, int N, int j)
+		{
+			M = new int[N-1][];
+			for (int k = 0; k < (N-1); k++)
+					M[k] = new int [N-1];
+			for(int i = 1; i < N; i ++)
 			{
-				return A[0][0];
-			}
-			else if (n ==2)
-			{
-				det = ((A[0][0]*A[1][1])-(A[0][1]*A[1][0]));
-			}
-			else 
-			{
-				for (int f = 0; f < n; f++){
-				//Getting minor of A[0][f]
-				getMinor(A, temp, 0, f, n);
-				det += sign * A[0][f]*determinant(temp, n-1);
-				
-				//Terms are added with alternate sign
-				sign = -sign;
+				int j1 = 0;
+				for(int j2 = 0; j2< N; j2++ )
+				{
+					if(j2 ==j)
+						continue;
+					M[i-1][j1] = (int) A[i][j2];
+					j1++;
 				}
-			}				
-			return det;
+			}
+			return M;
 		}
-		//Display the matrix
-		public void display(int A[][], int i, int j ){
-			for (int row = 0; row < i; i++){
-					for (int col = 0; col < j; j++){
-						System.out.println(A[col][row]);
-					}
-			}			
+	//Calculate determinant recursively
+	public int determinant(int A[][], int N)
+	{
+		int cofactor;
+		//For 1x1 Matrix
+		if (N == 1)
+			cofactor = A[0][0];
+		//For 2x2 Matrix
+		else if (N == 2)
+			cofactor = A[0][0]*A[1][1]-A[1][0]*A[0][1];
+		//NxN matrix
+		else
+		{
+			cofactor = 0;
+			for(int j = 0; j < N; j++)
+			{
+				M = SubArray(A, N, j);
+				cofactor += Math.pow(-1, 1+j+1)*A[0][j]*determinant(M,N-1);
+			}
 		}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		return cofactor; 
 	}
-
 }
